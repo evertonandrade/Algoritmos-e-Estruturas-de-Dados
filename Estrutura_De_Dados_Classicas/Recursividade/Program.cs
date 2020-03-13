1da122d39d31e12ec0c9eb1bdb6e59a440e6fb72
Jose Everton da Fraga Andrade <1182125287@acd.grupotiradentes.com>
2020-03-12 21:14:38 -0300

    Exercicios de recursividade

---
+++ Estrutura_De_Dados_Classicas/Recursividade/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Recursividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Potenciacao(2, 3));
+            System.Console.WriteLine(Inverter(12345));
+        }
+
+        static int Potenciacao(int num_base, int num_expoente)
+        {
+            if (num_expoente==0)
+            {
+                return 1;
+            }
+                
+            return num_base * Potenciacao(num_base, num_expoente - 1);
+        }
+
+        static int Inverter(int num, int total = 0)
+        {
+            if(num == 0)
+            {
+                return total;
+            }
+            else
+            {
+                return Inverter(num / 10, total * 10 + num % 10);
+            }
+        }
+    }
+}
